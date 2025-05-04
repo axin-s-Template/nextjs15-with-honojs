95cc602207686588bc845b1f6b4a2411d12c9994
coderPerseus <snailrun160@gmail.com>
2025-05-05 00:34:11 +0800

    fix: add hono

---
+++ src/config/meta.ts
@@ -0,0 +1,12 @@
+// 网站元数据
+
+export const siteConfig = {
+  title: "next-with-hono",
+  description: "基于 Next.js 15  + Hono 的 starter 模版",
+  url: "https://nextjs.org",
+  author: "luckySnail",
+  keywords: ["Next.js", "React", "JavaScript", "nestjs", "luckySnail"],
+  ogImage: "https://nextjs.org/og.png",
+  twitterImage: "https://nextjs.org/twitter.png",
+  lang: "zh-CN",
+};
